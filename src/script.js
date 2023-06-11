f2f732f9ae1699092188362d64047d8ca87f8097
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-06-11 03:57:00 -0300

    add page button

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
-fetch('http://localhost:3131/', {
-    method: 'Get'
+const cosplay = document.getElementById('cosplay');
+
+cosplay.addEventListener('click', ()=> {
+    window.location.href = "./pages/cosplay.html";
 })
+
+
+
+
+
+
+// cosplay.addEventListener('click', () => {
+//     fetch('http://localhost:3131/cosplay')
+//         .then(res => {
+//             console.log(res);
+//         })
+//         .catch(error => console.log(error));
+// });
