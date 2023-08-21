526f2885556c7486ecfde3bcb0bf0fe222e609b7
Nahuel <131316681+Nahuel-Salvatierra@users.noreply.github.com>
2023-08-21 02:59:33 -0300

    feat: :art: Enhanced Interactivity & Artistry

--- src/script.js
+++ src/script.js
@@ -9,4 +9,19 @@ maquillaje.addEventListener('click',()=> {
     window.location.href = "./pages/maquillaje.html";
 })
 
+const collapseRead = document.getElementById('collapse-read')
+const asdf = collapseRead.getAttribute('aria-expanded')
+collapseRead.addEventListener('click', ()=>{
+    if (collapseRead.classList.contains('collapsed')) {
+        collapseRead.classList.remove("flip-horizontal-top");
+        collapseRead.classList.add("flip-horizontal-bottom");
+        collapseRead.firstChild.textContent = 'Mostrar más'
+
+    } else {
+        collapseRead.classList.remove("flip-horizontal-bottom");
+        collapseRead.classList.add("flip-horizontal-top");
+        collapseRead.firstChild.textContent = 'Mostrar menos'
+    }
+})
+
 
